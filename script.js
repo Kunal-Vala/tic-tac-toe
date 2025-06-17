473185a019836eb293b414c6f9485065cc64b3a4
Kunal-Vala <kunalvala2005@gmail.com>
2025-06-17 21:57:12 +0530

    Add gameboard function

---
+++ script.js
@@ -0,0 +1,22 @@
+const Gameboard = (() => {
+    const board = Array(9).fill(""); // ["", "", ..., ""]
+
+    const placeMarker = (index, symbol) => {
+        if (board[index] === "") {
+            board[index] = symbol;
+            return true;
+        }
+        return false; // spot already taken
+    };
+
+    const getBoard = () => board;
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    };
+
+    return { placeMarker, getBoard, reset };
+})();
+
